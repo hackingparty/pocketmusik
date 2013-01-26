c491b71742b70ec86b0314217fd1c963d985a99e
Timshel <timshel@R700.(none)>
2013-01-26 12:48:20 +0100

    Remove generated files

--- client/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/ast/source/pocketmusik/client/bin/AndroidManifest.xml : \
-/Users/ast/source/pocketmusik/client/AndroidManifest.xml \
--- client/bin/PocketMusik-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/ast/source/pocketmusik/client/bin/PocketMusik-debug-unaligned.apk : \
-/Users/ast/source/pocketmusik/client/bin/PocketMusik.ap_ \
-/Users/ast/source/pocketmusik/client/bin/classes.dex \
-/Users/ast/source/pocketmusik/client/libs/cordova-2.2.0.jar \
