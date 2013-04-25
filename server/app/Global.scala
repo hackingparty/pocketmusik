97fddfc22b2f621382c8a1af7a7d832c7afbd7d3
Alex S <bobylito@gmail.com>
2013-04-25 19:38:45 +0200

    Add message to notify about data scan

--- server/app/Global.scala
+++ server/app/Global.scala
@@ -4,6 +4,7 @@ import play.api._
 object Global extends GlobalSettings {
   
   override def onStart(app: Application) {
+    Logger.debug( "Looking for files in the specified folder...")
     utils.Indexer.index(controllers.Application.rootPath)
     Logger.debug( "\n   " + models.Directory.findAll().mkString("\n   "))
   }
