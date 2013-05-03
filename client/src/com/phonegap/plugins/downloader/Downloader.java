1e2b1f0c57dfe7ac16a0c2c4a5accddd7e6571ff
Alex S <bobylito@gmail.com>
2013-05-03 09:18:50 +0200

    Download folders (to clean)

--- client/src/com/phonegap/plugins/downloader/Downloader.java
+++ client/src/com/phonegap/plugins/downloader/Downloader.java
@@ -1,5 +1,6 @@
 package com.phonegap.plugins.downloader;
 
+import android.util.Log;
 import android.app.DownloadManager;
 import android.net.Uri;
 
@@ -16,7 +17,7 @@ public class Downloader extends Plugin {
 
 	@Override
 	public PluginResult execute(String action, JSONArray args, String callbackId) {
-		
+	    	
 		if (!action.equals("downloadFile")) 
 			return new PluginResult(PluginResult.Status.INVALID_ACTION);
 		
@@ -31,18 +32,28 @@ public class Downloader extends Plugin {
 			return this.downloadUrl(fileUrl, dirName, fileName);
 			
 		} catch (JSONException e) {
-			e.printStackTrace();
-			return new PluginResult(PluginResult.Status.JSON_EXCEPTION, e.getMessage());
+            String err = e.getMessage();
+            Log.e("CordovaDM", err);
+			return new PluginResult(PluginResult.Status.JSON_EXCEPTION, err);
+		} catch (Exception e) {
+            String err = e.getMessage();
+            Log.e("CordovaDM", err);
+			return new PluginResult(PluginResult.Status.ERROR, err);
 		}
+
 	}
 
 	private PluginResult downloadUrl(String fileUrl, String dirName, String fileName ) {
-		if( !new File(String.format("/sdcard%s/%s", dirName, fileName)).exists() ){
-			DownloadManager.Request r = new DownloadManager.Request( Uri.parse(Uri.encode(fileUrl, "/:")) );
-
-			r. setDestinationInExternalPublicDir( dirName, fileName );
+        File folder = this.createDirs("/sdcard/", dirName);
+        File file   = new File(folder, fileName);
+        Log.d("CordovaDM", file.toString());
+		if( !file.exists() ){
+			DownloadManager.Request r = 
+                new DownloadManager.Request( Uri.parse(Uri.encode(fileUrl, "/:")) )
+                                    //.setDestinationUri( Uri.fromFile(folder) )
+                                    .setDestinationInExternalPublicDir(dirName, fileName)
+			                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
 			r.allowScanningByMediaScanner();
-			r.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
 
 			DownloadManager dm = (DownloadManager) cordova.getActivity()
 				.getSystemService(android.content.Context.DOWNLOAD_SERVICE);
@@ -50,4 +61,38 @@ public class Downloader extends Plugin {
 		}
 		return new PluginResult(PluginResult.Status.OK, "yeah");
 	}
+
+    private File createDirs(String root, String dirPath ){
+        String current = root + "/" + dirPath;
+        File f = new File(current);
+        f.mkdirs();
+        return f;
+    }
+
+    /*
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                long downloadId = intent.getLongExtra( DownloadManager.EXTRA_DOWNLOAD_ID, 0 );
+                Query query = new Query();
+                query.setFilterById(enqueue);
+                Cursor c = dm.query(query);
+                if (c.moveToFirst()) {
+                    int columnIndex = c.getColumnIndex(DownloadManager.COLUMN_STATUS);
+                    if (DownloadManager.STATUS_SUCCESSFUL == c.getInt(columnIndex)) {
+
+                        ImageView view = (ImageView) findViewById(R.id.imageView1);
+                        String uriString = c.getString(c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                        view.setImageURI(Uri.parse(uriString));
+                    }
+                }
+            }
+        }
+    };
+
+    registerReceiver(receiver, new IntentFilter(
+                DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    */
 }
